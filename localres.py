8824367cc76ca8d389415930970f2d535d3bbfcc
Guillaume Gaullier <guillaume@gaullier.org>
2019-12-03 15:07:24 -0700

    Added command-line parsing

--- localres.py
+++ localres.py
@@ -4,6 +4,9 @@
 import mrcfile as mrc
 import numpy as np
 import matplotlib.pyplot as plt
+import click
+
+# Building blocks
 
 def load_data(mrc_file):
     with mrc.open(mrc_file) as my_file:
@@ -25,3 +28,20 @@ def plot_histogram(values, title = 'Dataset 1'):
     ax.grid(True)
     plt.show()
     return fig
+
+# Command-line tool made from the buidling blocks
+
+CONTEXT_SETTINGS = dict(help_option_names = ['-h', '--help'])
+
+@click.command(context_settings = CONTEXT_SETTINGS)
+@click.argument('relion_locres', metavar = '<relion_locres.mrc>')
+@click.argument('mask', metavar = '<mask.mrc>')
+def cli(relion_locres, mask):
+    """Plots a histogram of local resolution values from RELION files.
+
+    For meaningful results, the mask.mrc file must be the one used for the 3D refinement and post-processing jobs that produced the relion_locres.mrc file."""
+    values = compute_values(relion_locres, mask)
+    plot_histogram(values)
+
+if __name__ == '__main__':
+    cli()
